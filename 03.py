8d0728ba1eaaafb6ddf420ee639d8da3dc4976b9
cyl606 <einmannderwash@gmail.com>
2025-10-21 21:27:20 +0800

    03

---
+++ 03.py
@@ -0,0 +1,18 @@
+###int
+print(0b100) #二进制
+print(0o100) #eight
+print(100) #ten
+print(0x100) #sixteen
+
+###float
+print(123.456) #float
+print(1.23456e2) #scientific notation
+
+###string
+print("Hello,World!")
+
+###boolean
+print(True)
+
+
+
